a4a586299444471c235a6dd9520ef46c86b79231
MatthewLuthi <mattluthi97@gmail.com>
2020-04-30 13:30:36 +1200

    Now using state to manage the score and number of responses

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import QuestionBox from "./components/QuestionBox";
 
 class TransformersQuiz extends Component {
     state = {
-        questionBank: []
+        questionBank: [],
+        score: 0,
+        responses: 0
     };
 
     /**
@@ -18,6 +20,18 @@ class TransformersQuiz extends Component {
             this.setState({
                 questionBank: question
             });
+        });
+    };
+
+    computeAnswer = (answer, correctAnswer) => {
+        if (answer === correctAnswer) {
+            this.setState({
+                score: this.state.score + 1
+            });
+        }
+
+        this.setState({
+            responses: this.state.responses < 5 ? this.state.responses + 1 : 5
         });
     };
 
@@ -33,15 +47,19 @@ class TransformersQuiz extends Component {
         return (
             <div className="Container">
                 <div className="title">Transformers Quiz</div>
-                {this.state.questionBank.length > 0 && 
+                {this.state.questionBank.length > 0 &&
+                    this.state.responses < 5 &&
                     this.state.questionBank.map(
                         ({question, answers, correct, questionId}) => (
                             <QuestionBox 
                                 question={question} 
                                 options={answers} 
                                 key={questionId} 
+                                selected={answer => this.computeAnswer(answer, correct)}
                             />
                         ))}
+
+                {this.state.responses === 5 ? (<h2>{this.state.score}</h2>) : null}
             </div>
         )
     }
