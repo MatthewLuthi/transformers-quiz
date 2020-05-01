fd9c3c13c9983f469c0f0729700075341782f144
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-01 04:40:07 +0000

    Deploy MatthewLuthi/transformers-quiz to github.com/MatthewLuthi/transformers-quiz.git:gh-pages

---
+++ precache-manifest.879f7f70a3a725262a7069e4d1c0e46f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93257a96470a0924468b33dece46f7ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2392fc82119ba9f8f9d5",
+    "url": "/static/css/main.0f93a89c.chunk.css"
+  },
+  {
+    "revision": "89523ec066da7f7d5e19",
+    "url": "/static/js/2.4286cada.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4286cada.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2392fc82119ba9f8f9d5",
+    "url": "/static/js/main.e76ff44f.chunk.js"
+  },
+  {
+    "revision": "05e1d1913fa03ddc9a21",
+    "url": "/static/js/runtime-main.fac00885.js"
+  }
+]);
